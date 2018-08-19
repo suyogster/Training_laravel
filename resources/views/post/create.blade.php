e8483022b4dbf20359b0b46333754e90e8d304f7
suyogster <intensenotes@gmail.com>
2018-08-19 15:40:42 +0545

    Slug Form Added

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -13,6 +13,9 @@
             {{ Form::label('title', 'Title:') }}
             {{Form::text('title',null, array('class' => 'form-control'))}}
 
+            {{ Form::label('slug', "Slug: ")}}
+            {{ Form::text('slug',null, array('class' => 'form-control', 'required' => ' ', 'minlength' => '5', 'maxlength' => '255'))}}
+
             {{Form::label('body', 'Post Body: ')}}
             {{Form::textarea('body', 'null', array('class' => 'form-control'))}}
 
