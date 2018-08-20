9fc6f9543e483de0a493e512c344335d4002bf33
suyogster <intensenotes@gmail.com>
2018-08-20 19:36:05 +0545

    user activate added

---
+++ app/Notifications/UserActivate.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserActivate extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+    
+        return (new MailMessage)
+        ->from(env('ADMIN_MAIL_ADDRESS'))
+        ->subject('Activate Account!')
+        ->greeting(sprintf('Hi, %s', $this->user->name))
+        ->line('We just noticed that you created a new account. You will need to activate your account to sign in into this account.')
+        ->action('Activate', route('activate', [$this->user->token]))
+        ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
