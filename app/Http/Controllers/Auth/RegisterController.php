1d77c04f0d25b041b630b6c37a118974b41a73d0
suyogster <intensenotes@gmail.com>
2018-08-20 19:35:50 +0545

    username added in validator

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
+            'username' => 'required|string|max:20|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -65,8 +66,48 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'token' => str_random(40) . time(),
         ]);
+        $user->notify(new UserActivation($user));
+
+        return $user;
+    }
+
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        return redirect()->route('login')
+            ->with(['success' => 'Congratulations! your account is registered, you will shortly receive an email to activate your account.']);
+    }
+
+    /**
+     * @param $token
+     */
+    public function activate($token = null)
+    {
+        $user = User::where('token', $token)->first();
+
+        if (empty($user)) {
+            return redirect()->to('/')
+                ->with(['error' => 'Your activation code is either expired or invalid.']);
+        }
+
+        $user->update(['token' => null, 'active' => User::ACTIVE]);
+
+        return redirect()->route('login')
+            ->with(['success' => 'Congratulations! your account is now activated.']);
     }
 }
