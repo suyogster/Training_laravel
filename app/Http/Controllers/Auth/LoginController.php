266d2c1c9e31ecbd2fd15bcded1509a4c1579469
suyogster <intensenotes@gmail.com>
2018-08-20 19:35:26 +0545

    Login Controller Implemeneted

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,58 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $field = $this->field($request);
+
+        return [
+            $field => $request->get($this->username()),
+            'password' => $request->get('password'),
+            'active' => User::ACTIVE,
+        ];
+    }
+
+    /**
+     * Determine if the request field is email or username.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    public function field(Request $request)
+    {
+        $email = $this->username();
+
+        return filter_var($request->get($email), FILTER_VALIDATE_EMAIL) ? $email : 'username';
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $field = $this->field($request);
+
+        $messages = ["{$this->username()}.exists" => 'The account you are trying to login is not activated or it has been disabled.'];
+
+        $this->validate($request, [
+            $this->username() => "required|exists:users,{$field},active," . User::ACTIVE,
+            'password' => 'required',
+        ], $messages);
+    }
+
+    public function getLogin(){
+
+        return view('auth.login');
+    }
 }
